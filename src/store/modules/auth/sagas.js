c0ef228c4a2ea898c8a9a15bc7c4f923985fb993
Sandro Santos <sandro.dev@gmail.com>
2020-01-04 17:34:39 -0300

    signUp Request on module auth of redux

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -4,7 +4,7 @@ import { toast } from 'react-toastify';
 import history from '~/services/history';
 import api from '~/services/api';
 
-import { signInSuccess, signFailure } from './actions';
+import { signInSuccess, signFailure, signUpRequest } from './actions';
 
 export function* signIn({ payload }) {
   try {
@@ -31,4 +31,25 @@ export function* signIn({ payload }) {
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+
+    history.push('/');
+  } catch (err) {
+    toast.error('Falha no cadastro, verifique seus dados!');
+    yield put(signFailure());
+  }
+}
+
+export default all([
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+]);
