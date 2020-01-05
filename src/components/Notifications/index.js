43ce8f404ae8e8a96648f16605914d941cc6b643
Sandro Santos <sandro.dev@gmail.com>
2020-01-05 17:30:55 -0300

    library react-perfect-scrollbar and configuration, to create a scrollbar in notification list

--- src/components/Notifications/index.js
+++ src/components/Notifications/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 import { MdNotifications } from 'react-icons/md';
 
-import { Container, Badge, NotificationList, Notification } from './styles';
+import {
+  Container,
+  Badge,
+  NotificationList,
+  Scroll,
+  Notification,
+} from './styles';
 
 export default function Notifications() {
   return (
@@ -11,23 +17,42 @@ export default function Notifications() {
         <MdNotifications color="#7159c1" size={20} />
       </Badge>
       <NotificationList>
-        <Notification unread>
-          <p>Você possui um novo agendamento para amanhã</p>
-          <time>há 2 dias</time>
-          <button type="button">Marcar como lida</button>
-        </Notification>
+        <Scroll>
+          <Notification unread>
+            <p>Você possui um novo agendamento para amanhã</p>
+            <time>há 2 dias</time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
 
-        <Notification unread>
-          <p>Você possui um novo agendamento para amanhã</p>
-          <time>há 2 dias</time>
-          <button type="button">Marcar como lida</button>
-        </Notification>
+          <Notification unread>
+            <p>Você possui um novo agendamento para amanhã</p>
+            <time>há 2 dias</time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
 
-        <Notification>
-          <p>Você possui um novo agendamento para amanhã</p>
-          <time>há 2 dias</time>
-          <button type="button">Marcar como lida</button>
-        </Notification>
+          <Notification>
+            <p>Você possui um novo agendamento para amanhã</p>
+            <time>há 2 dias</time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+          <Notification unread>
+            <p>Você possui um novo agendamento para amanhã</p>
+            <time>há 2 dias</time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+
+          <Notification unread>
+            <p>Você possui um novo agendamento para amanhã</p>
+            <time>há 2 dias</time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+
+          <Notification>
+            <p>Você possui um novo agendamento para amanhã</p>
+            <time>há 2 dias</time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+        </Scroll>
       </NotificationList>
     </Container>
   );
