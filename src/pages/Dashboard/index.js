0c1d3bcb2287deb901e41730fea93313854c1ea4
Sandro Santos <sandro.dev@gmail.com>
2020-01-07 20:26:36 -0300

    Dashboard page and styles

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, Time } from './styles';
+import api from '~/services/api';
 
 export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong> 07 de Janeiro</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Sandro Santos</span>
+        </Time>
+
+        <Time available>
+          <strong>09:00</strong>
+          <span>Sandro Santos</span>
+        </Time>
+
+        <Time>
+          <strong>10:00</strong>
+          <span>Sandro Santos</span>
+        </Time>
+
+        <Time>
+          <strong>11:00</strong>
+          <span>Sandro Santos</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
